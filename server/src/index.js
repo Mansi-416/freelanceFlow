4eb0c78b1831dbfa47fe6bed3efe4c4b3f838aba
Mansi-416 <mansijain1143@gmail.com>
2026-05-16 11:38:36 +0530

    Added root route

--- server/src/index.js
+++ server/src/index.js
@@ -44,3 +44,6 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+app.get("/", (req, res) => {
+  res.send("FreelanceFlow Backend Running");
+});
